f0e6d5f1d135670ddaba30af1c972a66b1810e54
samuellee77 <hsl023@ucsd.edu>
2025-03-08 14:13:02 -0800

    several tiebreakers implemented

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import time
 
+from collections import Counter
 from data.classes.Board import Board
 from data.classes.Bot import Bot
+from tiebreakers import tiebreaker
 
 import importlib.util
 import os
@@ -11,8 +13,7 @@ def generate_round_robin(bot_names):
     for i in range(len(bot_names)):
         for j in range(i + 1, len(bot_names)):
             yield bot_names[i], bot_names[j]
-
-
+    
 if __name__ == "__main__":
     # get all bot files from bots folder
     bots = os.listdir("bots")
@@ -29,6 +30,7 @@ if __name__ == "__main__":
 
     bot_names = list(name_to_module.keys())
     scorecard = {bot: 0 for bot in bot_names}
+    match_results = {bot: {} for bot in bot_names}
     # play the round robin
     for bot1, bot2 in generate_round_robin(bot_names):
         bot1_module = name_to_module[bot1]
@@ -51,9 +53,15 @@ if __name__ == "__main__":
                 print(f"{bot2} wins!")
                 running = False
                 scorecard[bot2] += 3
+                match_results[bot1][bot2] = "L"
+                match_results[bot2][bot1] = "W"
             elif board.is_in_checkmate("white"):
                 print(f"{bot1} wins!")
                 running = False
                 scorecard[bot1] += 3
-
+                match_results[bot1][bot2] = "W"
+                match_results[bot2][bot1] = "L"
+            # add draw condition later
+    scorecard = tiebreaker(scorecard, match_results)
     print(scorecard)
+    print(match_results)
